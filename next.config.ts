417e7bc3e11b992696dc62063367e73c5c191314
AnvitaAnanya <anvitaananya93@gmail.com>
2026-06-15 07:46:21 +0530

    Direct API calls to Railway

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"}/:path*`,
-      },
-      {
-        source: "/api/:path*/",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000"}/:path*/`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
